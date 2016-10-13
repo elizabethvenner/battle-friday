723e4ea3bdb54ae436a7f8c893d5cf52a173ee87
elizabethvenner <elizabethvenner@googlemail.com>
2016-10-13 10:20:07 +0100

    Viewing hit points

--- spec/features/name_submission_spec.rb
+++ spec/features/name_submission_spec.rb
@@ -1,5 +1,5 @@
 
-RSpec.feature "Name submission", :type => :feature do
+feature "Name submission", :type => :feature do
   scenario "The players fill in their names" do
     visit "/"
 
@@ -8,5 +8,17 @@ RSpec.feature "Name submission", :type => :feature do
     click_button "Start battle!!!"
 
     expect(page).to have_text("Elizabeth vs Malinna")
+  end
+end
+
+feature "Hit points", :type => :feature do
+  scenario "Interface allows users to view each other's hit points" do
+    visit "/"
+
+    fill_in :player1, :with => "Elizabeth"
+    fill_in :player2, :with => "Malinna"
+    click_button "Start battle!!!"
+
+    expect(page).to have_text("Malinna's hit points: 100")
   end
 end
